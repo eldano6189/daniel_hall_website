c304f3d631d59db95b11aa73636c761a4408cc73
eldano6189 <eldano6189@gmail.com>
2022-07-03 22:51:22 +0100

    rectified spelling mistakes

--- data/AboutMe.js
+++ data/AboutMe.js
@@ -6,19 +6,19 @@ export const AboutMe = [
   {
     header: "Who am I?",
     text: [
-      "My names Daniel Hall and i am a junior front-end react developer.",
-      "I have spent the last 4 years learning and implementing my skills on small projects for an array of companys and friends.",
+      "My names Daniel Hall and I am a junior front-end React developer.",
+      "I have spent the last 4 years learning and implementing my skills on small projects for an array of companies and friends.",
       "I enjoy the thrill of problem solving, with a high attention to the finer details.",
     ],
     model: <Head />,
   },
   {
-    header: "So what skills do i have?",
+    header: "So what skills do I have?",
     text: [
       "Majority of my time is spent using React and NextJS.",
-      "I prefure to use standard CSS to deepen my knowledge and understanding for the core concepts of styling.",
+      "I prefer to use standard CSS to deepen my knowledge and understanding for the core concepts of styling.",
       "Git is my weapon of choice for version control.",
-      "I also have a base knowledge of AWS Cognito, GSAP, Framer & ThreeJS,",
+      "I also have a base knowledge of AWS Cognito, GSAP, Framer & Three.js,",
     ],
     model: <MeSkills />,
   },
@@ -26,7 +26,7 @@ export const AboutMe = [
     header: "My hobbies.",
     text: [
       "I am an avid sportsman! I love to play an array of sports including football, squash, rugby, MMA and the list goes on.",
-      "Also i'm a husband and father of two, who i love to spend my time with.",
+      "Also I'm a husband and father of two, who I love to spend my time with.",
       "I am also a serving British soldier. I get the opportunity to see amazing places all over the globe and meet interesting people.",
     ],
     model: <Uniform />,
